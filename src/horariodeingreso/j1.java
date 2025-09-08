b1d739af9f78d8f9352933db9add40971ee4eab1
ALOIS <ALOIS@ARCE14>
2025-09-07 22:46:44 -0500

    agrege ingreso

--- src/horariodeingreso/j1.java
+++ src/horariodeingreso/j1.java
@@ -5,11 +5,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class j1 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -36,7 +42,31 @@ public class j1 extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(37, 148, 378, 105);
+		contentPane.add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		
+		JLabel lblNewLabel = new JLabel("ingreso de mercaderia");
+		lblNewLabel.setBounds(37, 22, 45, 13);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(37, 62, 45, 13);
+		contentPane.add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(266, 29, 85, 21);
+		contentPane.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(107, 19, 96, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
 
 	}
-
 }
